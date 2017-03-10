561b08091c639a624e86ed2f02b474eb5ab93a94
Josh Rodd <josh@rodd.us>
2017-03-10 00:03:54 -0500

    Backwards compatibile with bencode-1.0 now

--- bencode/__init__.py
+++ bencode/__init__.py
@@ -48,24 +48,18 @@ def decode_dict(x, f):
         r[k], f = decode_func[x[f]](x, f)
     return (r, f + 1)
 
-decode_func = {}
-decode_func['l'] = decode_list
-decode_func['d'] = decode_dict
-decode_func['i'] = decode_int
-decode_func['0'] = decode_string
-decode_func['1'] = decode_string
-decode_func['2'] = decode_string
-decode_func['3'] = decode_string
-decode_func['4'] = decode_string
-decode_func['5'] = decode_string
-decode_func['6'] = decode_string
-decode_func['7'] = decode_string
-decode_func['8'] = decode_string
-decode_func['9'] = decode_string
+decode_func = {
+    'l': decode_list,
+    'd': decode_dict,
+    'i': decode_int,
+}
+for x in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+    decode_func[x] = decode_string
 
 def bdecode(x):
-    if isinstance(x, bytes):
-        x = x.decode("ascii")
+    return bdecode_ascii(x if not isinstance(x, bytes) else x.decode("ascii"))
+
+def bdecode_ascii(x):
     try:
         r, l = decode_func[x[0]](x, 0)
     except (IndexError, KeyError, ValueError):
@@ -74,57 +68,119 @@ def bdecode(x):
         raise BTFailure("invalid bencoded value (data after valid prefix)")
     return r
 
+encode_func3 = {
+    int: lambda x: ''.join(('i', str(x), 'e')),
+    bool: lambda x: bencode(1) if x else bencode(0),
+    str: lambda x: ''.join((str(len(x)), ':', x)),
+    list:  lambda x:\
+       ''.join(('l', ''.join([bencode(i) for i in x]), 'e')),
+    dict: lambda x:\
+       ''.join(('d', ''.join([(''.join((bencode(k), bencode(v))))\
+       for k, v in sorted(x.items())]), 'e')),
+}
 
-encode_func = {}
+bencode3 = lambda x: encode_func3[type(x)](x)
 
-bencode = lambda x: encode_func[type(x)](x)
+encode_func3[bytes] = encode_func3[str]
+encode_func3[tuple] = encode_func3[list]
 
-encode_func[int] = lambda x: ''.join(('i', str(x), 'e'))
-encode_func[bool] = lambda x: bencode(1) if x else bencode(0)
-encode_func[str] = lambda x: ''.join((str(len(x)), ':', x))
+# For backwards compatibility only
+class Bencached(object):
+    __slots__ = ['bencoded']
+    def __init__(self, s):
+        self.bencoded = s
+
+def encode_bencached(x, r):
+    r.append(x.bencoded)
+
+encode_func3[Bencached] = lambda x: x.bencoded
+
+def encode_int(x, r):
+    r.extend(('i', str(x), 'e'))
+
+def encode_bool(x, r):
+    encode_int(1, r) if x else encode_int(0, r)
+
+def encode_string(x, r):
+    r.extend((str(len(x)), ':', x))
+
+def encode_list(x, r):
+    r.append('l')
+    for i in x:
+        encode_func[type(i)](i, r)
+    r.append('e')
+
+def encode_dict(x,r):
+    r.append('d')
+    for k, v in sorted(x.items()):
+        r.extend((str(len(k)), ':', k))
+        encode_func[type(v)](v, r)
+    r.append('e')
+
+encode_func = {
+    Bencached: encode_bencached,
+    int: encode_int,
+    bool: encode_bool,
+    str: encode_string,
+    list: encode_list,
+    dict: encode_dict,
+}
+
 encode_func[bytes] = encode_func[str]
-encode_func[list] = lambda x: ''.join(('l', ''.join([bencode(i) for i in x]), 'e'))
 encode_func[tuple] = encode_func[list]
-encode_func[dict] = lambda x: ''.join(('d', ''.join([(''.join((bencode(k), bencode(v)))) for k, v in sorted(x.items())]), 'e'))
 
 # For Python 2.x
 try:
+    encode_func3[unicode] = encode_func3[str]
     encode_func[unicode] = encode_func[str]
 except NameError:
     pass
 
 try:
     from types import BooleanType
+    encode_func3[BooleanType] = encode_func3[bool]
     encode_func[BooleanType] = encode_func[bool]
 except ImportError:
     pass
 
 try:
     from types import StringType
+    encode_func3[StringType] = encode_func3[str]
     encode_func[StringType] = encode_func[str]
 except ImportError:
     pass
 
 try:
     from types import IntType
+    encode_func3[IntType] = encode_func3[int]
     encode_func[IntType] = encode_func[int]
 except ImportError:
     pass
 
 try:
     from types import LongType
+    encode_func3[LongType] = encode_func3[int]
     encode_func[LongType] = encode_func[int]
 except ImportError:
     pass
 
 try:
     from types import DictType
+    encode_func3[DictType] = encode_func3[dict]
     encode_func[DictType] = encode_func[dict]
 except ImportError:
     pass
 
 try:
     from types import TupleType
+    encode_func3[TupleType] = encode_func3[tuple]
     encode_func[TupleType] = encode_func[tuple]
 except ImportError:
     pass
+
+def bencode2(x):
+    r = []
+    encode_func[type(x)](x, r)
+    return ''.join(r)
+
+bencode = bencode3
